81328d9b88d30bde97d0ebef7d0953bf7a34959f
ChiragGhanshani <chiragghanshani@gmail.com>
2014-07-25 14:13:40 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Requirements:
+	Python 2.7.3
+	MPlayer (sudo apt-get install mplayer <- Debian)
+
+To install this script, simply add it to your path and call it from the terminal.
+
+To select the music file for the alarm, ensure that it is in the directory form which you are calling
+alarm.py.
+
+Planned Features:
+	Exact time specification for the alarm (eg. 8AM)
+	Play playlists of music (possibly based on the day of the week)
+	auto snooze
+	{more features will be added as I think of them}
---
+++ alarm.py
@@ -0,0 +1,14 @@
+import time
+from subprocess import call
+
+hours = raw_input('Please input the number of hours you would like the alarm to wait before ringing')
+minutes = raw_input('please input the number of minutes you would like the alarm to wait before ringing')
+music = ('please input the name of the music file you would like to play when the alarm rings')
+
+hours = int(hours)
+minutes = int(minutes)
+seconds = (hours * 3200) + (minutes * 60)
+
+time.sleep(seconds)
+
+call("mplayer -loop 0 " + music, shell=True)
