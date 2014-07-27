055be81cd37559e51407dad366fc65fcd8b77dbd
ChiragGhanshani <chiragghanshani@gmail.com>
2014-07-27 12:29:36 -0500

    added playlist feature

--- README.md
+++ README.md
@@ -7,8 +7,17 @@ To install this script, simply add it to your path and call it from the terminal
 To select the music file for the alarm, ensure that it is in the directory form which you are calling
 alarm.py.
 
+To use the playlist feature, the name of a text file containing all of absolute paths for the songs
+you would like to play must be be given to the program when it asks for the playlist. One easy way to
+make make this file is to use 'find'.
+	Example: if all of the music files you want in your playlist are in your ~/Music/ directory and
+		all of them are mp3 files, you can type the following into your command line to quickly
+		generate a playlist:
+			'find /home/<yourHomeDirectory>/Music/ -iname '*.mp3' > playlist.txt
+		playlist.txt can then be given to alarm.py when it asks for the name of the playlist file.
+
 Planned Features:
-	Exact time specification for the alarm (eg. 8AM)
-	Play playlists of music (possibly based on the day of the week)
+	Exact time specification for the alarm (eg. 8AM) <in progress>
+	Play playlists of music (possibly based on the day of the week) <feature added>
 	auto snooze
 	{more features will be added as I think of them}
--- alarm.py
+++ alarm.py
@@ -3,14 +3,22 @@
 import time
 from subprocess import call
 
-hours = raw_input('Please input the number of hours you would like the alarm to wait before ringing')
-minutes = raw_input('please input the number of minutes you would like the alarm to wait before ringing')
-music = raw_input('please input the name of the music file you would like to play when the alarm rings')
+def timer():
+	hours = raw_input('Please input the number of hours you would like the alarm to wait before ringing ')
+	minutes = raw_input('Please input the number of minutes you would like the alarm to wait before ringing ')
+	seconds = raw_input('Please input the number of seconds you would like the alarm to waid before ringing ')
+	option = raw_input('Please input \'p\' if you would like to use a playlist for the alarm or \'m\' if you would like to use a song ')
+	hours = int(hours)
+	minutes = int(minutes)
+	seconds = int(seconds)
+	timer = (hours * 3200) + (minutes * 60) + seconds
+	if option == 'p' or option == 'P':
+		music = raw_input('please input the name of the playlist text file you would like to use ')
+		time.sleep(timer)
+		call('mplayer -loop 0 -shuffle -playlist ' + music, shell=True)		
+	else:
+		music = raw_input('Please input the name of the music file you would like to play when the alarm rings ')
+		time.sleep(timer)
+		call("mplayer -loop 0 " + music, shell=True)
 
-hours = int(hours)
-minutes = int(minutes)
-seconds = (hours * 3200) + (minutes * 60)
-
-time.sleep(seconds)
-
-call("mplayer -loop 0 " + music, shell=True)
+timer()
