d9a0e2db2d033273fc661d3ceed22fe5493a9457
ChiragGhanshani <chiragghanshani@gmail.com>
2014-08-03 23:59:45 -0500

    Process editing

--- alarm.py
+++ alarm.py
@@ -2,6 +2,9 @@
 
 import time
 from subprocess import call
+from multiprocessing import Process
+
+control = True
 
 def timer():
 	hours = raw_input('Please input the number of hours you would like the alarm to wait before ringing ')
@@ -15,11 +18,17 @@ def timer():
 	if option == 'p' or option == 'P':
 		music = raw_input('please input the name of the playlist text file you would like to use ')
 		time.sleep(timer)
-		call('mplayer -loop 0 -shuffle -playlist ' + music, shell=True)		
+		Snooze = Process(target=snooze)
+		Snooze.start()
+		Snooze.join()
+		call('mplayer -slave -input file=mplayerControl -loop 0 -shuffle -playlist ' + music, shell=True)	
 	else:
 		music = raw_input('Please input the name of the music file you would like to play when the alarm rings ')
 		time.sleep(timer)
-		call("mplayer -loop 0 " + music, shell=True)
+		Snooze = Process(target=snooze)
+		Snooze.start()
+		Snooze.join()
+		call("mplayer -slave -input file=mplayerControl -loop 0 " + music, shell=True)
 
 def alarm():
 	hour = int(raw_input('Please enter the hour of day at which you would like to wake up in 24-hour HH format'))
@@ -35,12 +44,38 @@ def alarm():
 		if awaken.tm_hour == hour and awaken.tm_min == minute:
 			x = x + 1
 	if option == 'p' or option == 'P':
-		call('mplayer -loop 0 -shuffle -playlist ' + music, shell=True)
+		Snooze = Process(target=snooze)
+		Snooze.start()
+		Snooze.join()
+		call('mplayer -slave -input file=mplayerControl -loop 0 -shuffle -playlist ' + music, shell=True)
 	else:
-		call('mplayer -loop 0 ' + music, shell=True)
+		Snooze = Process(target=snooze)
+		Snooze.start()
+		Snooze.join()
+		call('mplayer -slave -input file=mplayerControl -loop 0 ' + music, shell=True)
 
-choice = raw_input('Please enter 1 to use the alarm function or 2 to use the timer')
+def snooze():
+	while control:
+		time.sleep(60)
+		call("echo p > mplayerControl", shell=True)
+		time.sleep(300)
+		call("echo p > mplayerControl", shell=True)
+	call('echo q > mplayerControl', shell=True)
+
+choice = int(raw_input('Please enter 1 to use the alarm function or 2 to use the timer'))
 if choice == 2:
-	timer()
+	Timer = Process(target=timer)
+	Timer.start()
+	Timer.join()
+	time.sleep(100)
+	end = raw_input()
+	if end == 'q' or end == 'Q':
+		control = False
 else:
-	alarm()
+	Alarm = Process(target=alarm)
+	Alarm.start()
+	alarm.join()
+	time.sleep(100)
+	end = raw_input()
+	if end == 'q' or end == 'Q':
+		control = False
